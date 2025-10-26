575afb1f209732ddf237dba2be36ac2cbd659626
iv4n89 <ivan.betanzos89@gmail.com>
2025-10-26 04:03:24 +0100

    fix: moved cors bypass service url to whateverorigin

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { Routes } from './ui/common/router/index.routes';
 import { QueryProviderClient } from './ui/common/util/query-client/query-client-provider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
     <>
       <QueryProviderClient>
         <RouterProvider router={Routes} />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+        />
       </QueryProviderClient>
     </>
   );
