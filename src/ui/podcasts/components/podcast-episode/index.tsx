90a5fc27b8aed71ece5dcc49e53645fc0d07ac88
iv4n89 <ivan.betanzos89@gmail.com>
2025-10-26 06:24:47 +0100

    feat: episode view logic

---
+++ src/ui/podcasts/components/podcast-episode/index.tsx
@@ -0,0 +1,28 @@
+import { ShadowedBox } from '@/ui/common/components/shadowed-box';
+import { sanitizeString } from '@/ui/common/util/sanitize-string';
+import styles from './podcast-episode.module.css';
+
+interface Props {
+  name: string;
+  description: string;
+  audioUrl: string;
+}
+
+export const PodcastEpisode = ({ audioUrl, description, name }: Props) => {
+  return (
+    <ShadowedBox className={styles.podcast_episode__container}>
+      <div>
+        <h2>{name}</h2>
+        <div
+          className={styles.podcast_episode__description}
+          dangerouslySetInnerHTML={{ __html: sanitizeString(description) }}
+        ></div>
+      </div>
+      <audio
+        src={audioUrl}
+        controls
+        className={styles.podcast_episode__audio_player}
+      />
+    </ShadowedBox>
+  );
+};
--- src/ui/podcasts/views/episode.tsx
+++ src/ui/podcasts/views/episode.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { PodcastDescriptionCard } from '../components/podcast-description-card';
 import { PodcastDetailsContainer } from '../components/podcast-details-container';
+import { PodcastEpisode } from '../components/podcast-episode';
 import { useEpisodeDetails } from '../hooks/use-episode-details';
 
 export default function Episode() {
@@ -9,7 +10,7 @@ export default function Episode() {
     episodeId: string;
   }>();
 
-  const { description, info } = useEpisodeDetails({
+  const { description, episode, info } = useEpisodeDetails({
     episodeId: episodeId ?? '',
     podcastId: podcastId ?? '',
   });
@@ -24,6 +25,11 @@ export default function Episode() {
         data={info}
         description={description || ''}
         isLinksActive
+      />
+      <PodcastEpisode
+        name={episode?.trackName || ''}
+        description={episode?.description || ''}
+        audioUrl={episode?.episodeUrl || ''}
       />
     </PodcastDetailsContainer>
   );
